b0b6977c3125d51aa302a268f9bb2aee048224eb
Mohammed Farhan <51290906+mfarhanz@users.noreply.github.com>
2024-03-07 12:37:46 -0330

    Delete SiteStatusChecker/site_status_checker directory

--- SiteStatusChecker/site_status_checker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from site_status_checker import views
-
-urlpatterns = [
-    path("", views.default_check, name="default_check"),
-    path("<url>", views.check_url, name="check_url"),
-]
